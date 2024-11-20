b556a35c6c163d31c06dd30b178cd74466e6ee80
EngYota <ayanada816@gmail.com>
2024-11-20 10:12:46 +0200

    First

---
+++ js/main.js
@@ -0,0 +1,12 @@
+;(function($){
+
+    $(document).ready(function(){
+     
+    })
+    //========== COUNTER UP============= //
+// const ucounter = $('.counter');
+// if (ucounter.length > 0) {
+//  ucounter.countUp();  
+// };
+})(jQuery);
+
